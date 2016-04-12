32a034d9220ed06f2c1e750a63077ca62245a211
Bogdan Kobylinsky <92bogdan@gmail.com>
2016-04-12 21:39:45 +0300

    Add testSubmitIncorrectTelephoneAndEmail

--- src/test/java/ContactUsFormTest.java
+++ src/test/java/ContactUsFormTest.java
@@ -49,7 +49,9 @@ public class ContactUsFormTest extends BaseContactUsTest {
         // Success popup is absent
         assertFalse(isElementPresent(XPATH_SUBMITION_SUCCESS_ALERT));
         assertTrue(isElementPresent(XPATH_INPUT_MAIL + XPATH_ALERT_SUFFIX));
-        assertTrue(getValue(XPATH_INPUT_MAIL + XPATH_ALERT_SUFFIX).contains("Invalid email address"));
+
+        final String mailErrorText = getValue(XPATH_INPUT_MAIL + XPATH_ALERT_SUFFIX);
+        assertTrue("Error: " + mailErrorText, mailErrorText.contains("Invalid email address"));
     }
 
     @Test
@@ -62,8 +64,9 @@ public class ContactUsFormTest extends BaseContactUsTest {
         setValue(XPATH_INPUT_COMPANY, "GL");
         setValue(XPATH_INPUT_MESSAGE, "_");
 
-        // Check that success popup is absent
+        // Auccess popup is absent
         assertFalse(isElementPresent(XPATH_SUBMITION_SUCCESS_ALERT));
+        // Alert is absent
         assertFalse(isElementPresent(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX));
 
         // Submit
@@ -71,9 +74,43 @@ public class ContactUsFormTest extends BaseContactUsTest {
 
         // Success popup is absent
         assertFalse(isElementPresent(XPATH_SUBMITION_SUCCESS_ALERT));
+        // Alert is present
         assertTrue(isElementPresent(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX));
-        final String errorText = getValue(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX);
-        assertTrue("Error: " + errorText, errorText.contains("Invalid phone number"));
+
+        final String phoneErrorText = getValue(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX);
+        assertTrue("Error: " + phoneErrorText, phoneErrorText.contains("Invalid phone number"));
+    }
+
+    @Test
+    public void testSubmitIncorrectTelephoneAndEmail() {
+        // Setting incorrect email
+        setValue(XPATH_INPUT_NAME, "Bogdan");
+        setValue(XPATH_INPUT_MAIL, "Incorrect.email");
+        setValue(XPATH_INPUT_TELEPHONE, "INcorrect telephone");
+        setValue(XPATH_INPUT_COUNTRY, "KIEV");
+        setValue(XPATH_INPUT_COMPANY, "GL");
+        setValue(XPATH_INPUT_MESSAGE, "_");
+
+        // Auccess popup is absent
+        assertFalse(isElementPresent(XPATH_SUBMITION_SUCCESS_ALERT));
+        // Alerts are absent
+        assertFalse(isElementPresent(XPATH_INPUT_MAIL + XPATH_ALERT_SUFFIX));
+        assertFalse(isElementPresent(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX));
+
+        // Submit
+        clickSubmit();
+
+        // Success popup is absent
+        assertFalse(isElementPresent(XPATH_SUBMITION_SUCCESS_ALERT));
+        // Alerts are present
+        assertFalse(isElementPresent(XPATH_INPUT_MAIL + XPATH_ALERT_SUFFIX));
+        assertFalse(isElementPresent(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX));
+
+        final String mailErrorText = getValue(XPATH_INPUT_TELEPHONE + XPATH_ALERT_SUFFIX);
+        assertTrue("Error: " + mailErrorText, mailErrorText.contains("Invalid email address"));
+
+        final String phoneErrorText = getValue(XPATH_INPUT_MAIL + XPATH_ALERT_SUFFIX);
+        assertTrue("Error: " + phoneErrorText, phoneErrorText.contains("Invalid phone number"));
     }
 
 }
